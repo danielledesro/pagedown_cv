fc9a7e1ecc21e0852b78300cff3895d0c9702714
danielledesro <ddesrochers1212@gmail.com>
2024-01-13 12:30:56 -0600

    removing a sheet from google, removing corresponding line from cv_printing_functions

--- cv_printing_functions.r
+++ cv_printing_functions.r
@@ -44,14 +44,12 @@ create_CV_object <-  function(data_location,
     cv$skills        <- read_gsheet(sheet_id = "skills")
     cv$text_blocks   <- read_gsheet(sheet_id = "text_blocks")
     cv$contact_info  <- read_gsheet(sheet_id = "contact_info")
-    cv$output        <- read_gsheet(sheet_id = "output")
   } else {
     # Want to go old-school with csvs?
     cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1)
     cv$skills       <- readr::read_csv(paste0(data_location, "skills.csv"), skip = 1)
     cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
     cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
-    cv$output       <- readr::read_csv(paste0(data_location, "output.csv"), skip = 1)
   }
 
 
